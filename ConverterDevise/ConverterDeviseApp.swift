5fc0982a7889729d0c589c503294f553a6eddaa7
kmlkhatim <kmlkhatim@gmail.com>
2023-03-28 23:59:34 +0100

    Initial Commit

---
+++ ConverterDevise/ConverterDeviseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterDeviseApp.swift
+//  ConverterDevise
+//
+//  Created by Kamal KHATIM on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConverterDeviseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
