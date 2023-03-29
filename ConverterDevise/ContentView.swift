e40793d8e49db099c86009c4040947020c0312c7
kmlkhatim <kmlkhatim@gmail.com>
2023-03-29 02:02:14 +0100

    For the converter

--- ConverterDevise/ContentView.swift
+++ ConverterDevise/ContentView.swift
@@ -8,15 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var amount = ""
+    @State private var fromCurrency = "USD"
+    @State private var toCurrency = "EUR"
+    @State private var conversionRate = 0.0
+    @State private var convertedAmount = 0.0
+    
+    let apiUrl = "https://api.exchangerate-api.com/v4/latest/"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+            Image("cd")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150, alignment: .center)
+                .clipped()
+            
+            NavigationView {
+                Form {
+                    Section(header: Text("Form")) {
+                        TextField("Amount", text: $amount)
+                            .keyboardType(.decimalPad)
+                        
+                        Picker("Currency", selection: $fromCurrency){
+                            Text("USD").tag("USD")
+                            Text("MAD").tag("MAD")
+                            Text("EUR").tag("EUR")
+                            Text("GEP").tag("GEP")
+                            Text("JPY").tag("JPY")
+                        }
+                        
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    Section(header: Text("To")) {
+                        Picker("Currency", selection: $toCurrency) {
+                            Text("USD").tag("USD")
+                            Text("MAD").tag("MAD")
+                            Text("EUR").tag("EUR")
+                            Text("GEP").tag("GEP")
+                            Text("JPY").tag("JPY")
+                        }
+                        .pickerStyle(.segmented)
+                        Text("\(convertedAmount, specifier: "%.2f")")
+                    }
+                }
+                .navigationTitle("Converter :")
+                .onAppear(perform: fetchConversionRate)
+            }
         }
-        .padding()
     }
+    
+    func fetchConversionRate() {
+        let urlString = "\(apiUrl)\(fromCurrency)"
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try?
+                    JSONDecoder().decode(ExchangeRateResponse.self, from: data) {
+                    DispatchQueue.main.async {
+                        conversionRate = decodedResponse.rates[toCurrency] ?? 0
+                        if let amount = Double(amount) {
+                            convertedAmount = amount * conversionRate
+                        }
+                    }
+                    return
+                }
+            }
+        }.resume()
+    }
+}
+
+struct ExchangeRateResponse: Codable {
+    let rates: [String: Double]
 }
 
 struct ContentView_Previews: PreviewProvider {
